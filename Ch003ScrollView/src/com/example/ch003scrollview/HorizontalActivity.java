6ea1066cd6d5b0b42a90b30b99346e135d873a59
treizeor <huabao.z@163.com>
2014-06-01 23:32:21 +0800

    ListActivity & ScrollView

---
+++ Ch003ScrollView/src/com/example/ch003scrollview/HorizontalActivity.java
@@ -0,0 +1,14 @@
+package com.example.ch003scrollview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HorizontalActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.horizontal);
+	}
+}
---
+++ Ch003ScrollView/src/com/example/ch003scrollview/VerticalActivity.java
@@ -0,0 +1,14 @@
+package com.example.ch003scrollview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VerticalActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.vertical);
+	}
+
+}
