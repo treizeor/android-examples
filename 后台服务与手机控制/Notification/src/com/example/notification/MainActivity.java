dbe2b288840fab3fffb348f941a60bed09e89b44
treizeor <huabao.z@163.com>
2014-06-06 23:55:21 +0800

    Notification

---
+++ 后台服务与手机控制/Notification/src/com/example/notification/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.notification;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	private Button button1;
+	private NotificationManager nm;
+	static final int NOTIFICATION_ID = 0x123;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		button1 = (Button) findViewById(R.id.button1);
+
+		button1.setOnClickListener(this);
+		nm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+	}
+
+	@SuppressLint("NewApi") @Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.button1:
+			// 创建一个启动其他Active的Intent
+			Intent intent = new Intent(MainActivity.this, OtherActivity.class);
+			PendingIntent pi = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
+			Notification notify = new Notification.Builder(this)
+				.setAutoCancel(true)
+				.setTicker("您有新的消息")
+				.setSmallIcon(R.drawable.ic_launcher)
+				//.setLargeIcon(R.drawable.ic_launcher)
+				
+				.setContentTitle("一条新通知")
+				.setContentText("努力码字吧~~~")
+				
+				.setDefaults(Notification.DEFAULT_SOUND|Notification.DEFAULT_LIGHTS)
+				.setWhen(System.currentTimeMillis())
+				.setContentIntent(pi)
+				.build();
+				
+			nm.notify(NOTIFICATION_ID, notify);
+			break;
+		}
+
+	}
+
+}
---
+++ 后台服务与手机控制/Notification/src/com/example/notification/OtherActivity.java
@@ -0,0 +1,13 @@
+package com.example.notification;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OtherActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+}
