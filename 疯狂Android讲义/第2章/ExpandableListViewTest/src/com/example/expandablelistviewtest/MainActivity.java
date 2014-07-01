aec6e898efc3f95247c7bfe8938054c0a86b12d6
treizeor <huabao.z@163.com>
2014-07-01 21:20:42 +0800

    扩展的ListView

--- 疯狂Android讲义/第2章/ExpandableListViewTest/src/com/example/expandablelistviewtest/MainActivity.java
+++ 疯狂Android讲义/第2章/ExpandableListViewTest/src/com/example/expandablelistviewtest/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.expandablelistviewtest;
 
-import android.R.string;
 import android.app.Activity;
-import android.app.ExpandableListActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -17,84 +20,103 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		ExpandableListAdapter adapter = new BaseExpandableListAdapter() {
-			
-			int[] logos = new int[]{
-				R.drawable.img1,
-				R.drawable.img2,
-				R.drawable.img3,
-			};
-			private String[] armTypes = new String[]
-			{"神族兵种", "虫族兵种", "人族兵种"};
-			private String[][] arms = new String[][]{
-					{"狂战士","龙骑士","黑暗圣堂","电兵"},
-					{"小狗","刺蛇","飞龙","自爆飞机"},
-					{"机枪兵","护士MM","幽灵","法师"}
-			};
-			
-			//private TextView getTextView()
-			
+
+			int[] logos = new int[] { R.drawable.ic_launcher,
+					R.drawable.ic_launcher, R.drawable.ic_launcher, };
+			private String[] armTypes = new String[] { "神族兵种", "虫族兵种", "人族兵种" };
+			private String[][] arms = new String[][] {
+					{ "狂战士", "龙骑士", "黑暗圣堂", "电兵" },
+					{ "小狗", "刺蛇", "飞龙", "自爆飞机" }, { "机枪兵", "护士MM", "幽灵", "法师" } };
+
+			private TextView getTextView() {
+				AbsListView.LayoutParams lp = new AbsListView.LayoutParams(
+						ViewGroup.LayoutParams.MATCH_PARENT, 64);
+				TextView textView = new TextView(MainActivity.this);
+				textView.setLayoutParams(lp);
+				textView.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);
+				textView.setPadding(36, 0, 0, 0);
+				textView.setTextSize(20);
+				return textView;
+			}
+
 			@Override
-			public boolean isChildSelectable(int groupPosition, int childPosition) {
+			public boolean isChildSelectable(int groupPosition,
+					int childPosition) {
 				// TODO Auto-generated method stub
 				return true;
 			}
-			
+
 			@Override
 			public boolean hasStableIds() {
 				// TODO Auto-generated method stub
 				return true;
 			}
-			
+
 			@Override
 			public View getGroupView(int groupPosition, boolean isExpanded,
 					View convertView, ViewGroup parent) {
 				// TODO Auto-generated method stub
-				return null;
+				LinearLayout ll = new LinearLayout(MainActivity.this);
+				ll.setOrientation(0);
+				ImageView logo = new ImageView(MainActivity.this);
+				logo.setImageResource(logos[groupPosition]);
+
+				ll.addView(logo);
+				TextView textView = getTextView();
+				textView.setText(getGroup(groupPosition).toString());
+				ll.addView(textView);
+				return ll;
 			}
-			
+
 			@Override
 			public long getGroupId(int groupPosition) {
 				// TODO Auto-generated method stub
 				return groupPosition;
 			}
-			
+
 			@Override
 			public int getGroupCount() {
 				// TODO Auto-generated method stub
 				return armTypes.length;
 			}
-			
+
 			@Override
 			public Object getGroup(int groupPosition) {
 				// TODO Auto-generated method stub
 				return armTypes[groupPosition];
 			}
-			
+
 			@Override
 			public int getChildrenCount(int groupPosition) {
 				// TODO Auto-generated method stub
 				return arms[groupPosition].length;
 			}
-			
+
 			@Override
 			public View getChildView(int groupPosition, int childPosition,
 					boolean isLastChild, View convertView, ViewGroup parent) {
 				// TODO Auto-generated method stub
-				return null;
+				TextView textView = getTextView();
+				textView.setText(getChild(groupPosition, childPosition)
+						.toString());
+				return textView;
 			}
-			
+
 			@Override
 			public long getChildId(int groupPosition, int childPosition) {
 				// TODO Auto-generated method stub
 				return childPosition;
 			}
-			
+
 			@Override
 			public Object getChild(int groupPosition, int childPosition) {
 				// TODO Auto-generated method stub
 				return arms[groupPosition][childPosition];
 			}
 		};
-		
+
+		ExpandableListView expandableListView = (ExpandableListView) findViewById(R.id.expandableListView1);
+		expandableListView.setAdapter(adapter);
+
 	}
 }
