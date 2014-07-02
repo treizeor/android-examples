3ea459e8c85812799c95c025c0e7dc3c03d71089
treizeor <huabao.z@163.com>
2014-07-02 23:09:11 +0800

    Handler更新ProgressBar

--- 疯狂Android讲义/第2章/ProgressBarTest/src/com/example/progressbartest/MainActivity.java
+++ 疯狂Android讲义/第2章/ProgressBarTest/src/com/example/progressbartest/MainActivity.java
@@ -11,20 +11,18 @@ public class MainActivity extends Activity {
 	int status = 0;
 	ProgressBar bar1, bar2, progressBar;
 	// 创建一个负责更新的进度的Handler
-	// Handler mHandler = new Handler();
-	// Runnable add = new Runnable() {
-	//
-	// @Override
-	// public void run() {
-	// // TODO Auto-generated method stub
-	// status = progressBar.getProgress() + 1;
-	// progressBar.setProgress(status);
-	// if (status < 100) {
-	// mHandler.postDelayed(add, 50);
-	// }
-	// }
-	// };
+	Handler mHandler2 = new Handler();
+	Runnable add = new Runnable() {
+		public void run() {
+			status = progressBar.getProgress() + 1;
+			progressBar.setProgress(status);
+			if (status < 100) {
+				mHandler2.postDelayed(add, 50);
+			}
+		}
+	};
 
+	// handleMessage方式
 	Handler mHandler = new Handler() {
 		public void handleMessage(Message msg) {
 			if (msg.what == 0x111) {
@@ -40,7 +38,10 @@ public class MainActivity extends Activity {
 		bar1 = (ProgressBar) findViewById(R.id.bar1);
 		bar2 = (ProgressBar) findViewById(R.id.bar2);
 		progressBar = (ProgressBar) findViewById(R.id.progressBar1);
-		// mHandler.post(add);
+		// post方式更新progressbar
+		mHandler2.post(add);
+		
+		// sendmessage方式更新progressbar
 		new Thread() {
 			public void run() {
 				while (status < 100) {
