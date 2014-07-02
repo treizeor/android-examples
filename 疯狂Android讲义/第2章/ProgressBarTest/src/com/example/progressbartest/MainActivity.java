d7d83a1c8867cccb1f866e5aaa734fa37c3677f3
treizeor <huabao.z@163.com>
2014-07-02 22:45:33 +0800

    Handler以post方式更新ProgressBar

--- 疯狂Android讲义/第2章/ProgressBarTest/src/com/example/progressbartest/MainActivity.java
+++ 疯狂Android讲义/第2章/ProgressBarTest/src/com/example/progressbartest/MainActivity.java
@@ -8,22 +8,23 @@ import android.widget.ProgressBar;
 
 public class MainActivity extends Activity {
 
-	// 该程序模拟填充长度为100的数组
-//	int[] data = new int[100];
-//	int hasData = 0;
-	private int progress = 0;
 	int status = 0;
 	ProgressBar bar1, bar2, progressBar;
 	// 创建一个负责更新的进度的Handler
-	Handler mHandler = new Handler(){
-		public void handlerMessage(Message msg) {
-			if (msg.what == 0x123) {
-				bar1.setProgress(status);
-				bar2.setProgress(status);
-				progressBar.setProgress(status);
+	Handler mHandler = new Handler();
+	Runnable add = new Runnable() {
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			status = progressBar.getProgress() + 1;
+			progressBar.setProgress(status);
+			if (status < 100) {
+				mHandler.postDelayed(add, 50);
 			}
 		}
 	};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -31,35 +32,6 @@ public class MainActivity extends Activity {
 		bar1 = (ProgressBar)findViewById(R.id.bar1);
 		bar2 = (ProgressBar)findViewById(R.id.bar2);
 		progressBar = (ProgressBar)findViewById(R.id.progressBar1);
-		new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				while (progress < 100) {
-					try {
-						Thread.sleep(100);
-						mHandler.sendEmptyMessage(0x123);
-						progress++;
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-				
-			}
-		}).start();
+		mHandler.post(add);
 	}
-	
-//	public int doWork() {
-////		data[hasDate++] = (int) (Math.random()*100);
-//		try {
-//			Thread.sleep(100);
-//			hasData++;
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		return hasData;
-//	}
 }
