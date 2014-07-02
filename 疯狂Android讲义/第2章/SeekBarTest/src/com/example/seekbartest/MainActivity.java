c5c6ebadb9c746eb4d9fb120d891d46b0a57c444
treizeor <huabao.z@163.com>
2014-07-02 23:47:02 +0800

    SeekBar拖动滑块

---
+++ 疯狂Android讲义/第2章/SeekBarTest/src/com/example/seekbartest/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.seekbartest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+		final ImageView imgView = (ImageView) findViewById(R.id.imgView);
+
+		seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				// TODO Auto-generated method stub
+				imgView.setAlpha(progress);
+			}
+		});
+	}
+}
