d5c41f6a0800ecbee0968a316cc1939d69c36f02
treizeor <huabao.z@163.com>
2014-07-11 23:37:35 +0800

    简单选择列表对话框

--- 疯狂Android讲义/第2章/AlertDialogTest/src/com/example/alertdialogtest/MainActivity.java
+++ 疯狂Android讲义/第2章/AlertDialogTest/src/com/example/alertdialogtest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.alertdialogtest;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
@@ -28,13 +29,31 @@ public class MainActivity extends Activity {
 		builder.create().show();
 	}
 
+	public void simpleList(View view) {
+		final String[] items = new String[] { "aaaa", "bbbb", "cccc" };
+		AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this)
+				.setTitle("简单列表对话框").setIcon(R.drawable.ic_launcher)
+				.setItems(items, new OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						// TODO Auto-generated method stub
+						show("你选择了" + items[which]);
+					}
+				});
+		setPositiveButton(builder);
+		setNegativeButton(builder);
+		builder.create().show();
+	}
+
 	private AlertDialog.Builder setPositiveButton(AlertDialog.Builder builder) {
 		return builder.setPositiveButton("确定", new OnClickListener() {
 
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
 				// TODO Auto-generated method stub
-				Toast.makeText(MainActivity.this, "你单击了确定", Toast.LENGTH_SHORT).show();
+				Toast.makeText(MainActivity.this, "你单击了确定", Toast.LENGTH_SHORT)
+						.show();
 			}
 		});
 	}
@@ -45,10 +64,14 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
 				// TODO Auto-generated method stub
-				Toast.makeText(MainActivity.this, "你单击了取消", Toast.LENGTH_SHORT).show();
+				Toast.makeText(MainActivity.this, "你单击了取消", Toast.LENGTH_SHORT)
+						.show();
 
 			}
 		});
 	}
 
+	private void show(String msg) {
+		Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+	}
 }
