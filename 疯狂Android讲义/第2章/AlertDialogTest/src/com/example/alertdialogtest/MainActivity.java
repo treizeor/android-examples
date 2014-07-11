699fc0e163d06942a153ae9aeb69ff583b3de353
treizeor <huabao.z@163.com>
2014-07-11 23:20:42 +0800

    简单对话框

---
+++ 疯狂Android讲义/第2章/AlertDialogTest/src/com/example/alertdialogtest/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.alertdialogtest;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	public void simple(View view) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this)
+				.setTitle("简单对话框").setIcon(R.drawable.ic_launcher)
+				.setMessage("简单对话框测试内容\n换行测试");
+		// 添加确定按钮
+		setPositiveButton(builder);
+		// 添加取消按钮
+		setNegativeButton(builder);
+		// 创建及展示
+		builder.create().show();
+	}
+
+	private AlertDialog.Builder setPositiveButton(AlertDialog.Builder builder) {
+		return builder.setPositiveButton("确定", new OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				Toast.makeText(MainActivity.this, "你单击了确定", Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+
+	private AlertDialog.Builder setNegativeButton(AlertDialog.Builder builder) {
+		return builder.setNegativeButton("取消", new OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				Toast.makeText(MainActivity.this, "你单击了取消", Toast.LENGTH_SHORT).show();
+
+			}
+		});
+	}
+
+}
