5c289f851e71f2f5d4e5c49148fd42bb6cbdbcd6
treizeor <huabao.z@163.com>
2014-07-16 21:31:52 +0800

    对话框风格的窗口

---
+++ 疯狂Android讲义/第2章/DialogTheme/src/com/example/dialogtheme/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.dialogtheme;
+// AndroidManifest.xml 配置 android:theme="@android:style/Theme.Dialog
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Button button = (Button)findViewById(R.id.button1);
+		button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				finish();
+			}
+		});
+
+	}
+}
