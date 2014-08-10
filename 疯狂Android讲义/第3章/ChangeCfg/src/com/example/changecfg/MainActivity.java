b2aca9eadaf2e7094e9f1bcdf72f4a7047cac502
treizeor <huabao.z@163.com>
2014-08-10 19:40:22 +0800

    监听屏幕方向改变

--- 疯狂Android讲义/第3章/ChangeCfg/src/com/example/changecfg/MainActivity.java
+++ 疯狂Android讲义/第3章/ChangeCfg/src/com/example/changecfg/MainActivity.java
@@ -14,31 +14,28 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Button bn = (Button) findViewById(R.id.button1);
+		Button bn = (Button)findViewById(R.id.button1);
 		bn.setOnClickListener(new View.OnClickListener() {
-
+			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				Configuration cfg = getResources().getConfiguration();
 				if (cfg.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-					MainActivity.this
-							.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+					MainActivity.this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 				}
 				if (cfg.orientation == Configuration.ORIENTATION_PORTRAIT) {
-					MainActivity.this
-							.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+					MainActivity.this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 				}
 			}
 		});
 	}
-
+	
 	@Override
 	public void onConfigurationChanged(Configuration newConfig) {
 		// TODO Auto-generated method stub
 		super.onConfigurationChanged(newConfig);
-		String screen = newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE ? "横向屏幕"
-				: "竖向屏幕";
-		System.out.println("屏幕方向改变为" + screen);
+		String screen = newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE?"横向屏幕":"竖向屏幕";
+		Toast.makeText(MainActivity.this, "屏幕方向改变为"+screen, Toast.LENGTH_LONG).show();
 	}
 }
