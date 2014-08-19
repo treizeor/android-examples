6472f52df3aa7963addd952f71486d7ce28cc3fd
treizeor <huabao.z@163.com>
2014-08-20 00:23:11 +0800

    显示图书详情的Fragment

---
+++ 疯狂Android讲义/第4章/FragmentTest/src/com/example/fragmenttest/SelectBookActivity.java
@@ -0,0 +1,26 @@
+package com.example.fragmenttest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SelectBookActivity extends Activity implements
+		BookListFragment.Callbacks {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public void onItemSelected(Integer id) {
+		// TODO Auto-generated method stub
+		Bundle arguments = new Bundle();
+		arguments.putInt(BookDetailFragment.ITEM_ID, id);
+		BookDetailFragment fragment = new BookDetailFragment();
+		fragment.setArguments(arguments);
+		getFragmentManager().beginTransaction()
+				.replace(R.id.book_detail_container, fragment).commit();
+	}
+}
