36a6b1841825908142c8db01399a2ca1bce87ae3
treizeor <huabao.z@163.com>
2014-08-24 15:39:18 +0800

    返回系统桌面

---
+++ 疯狂Android讲义/第5章/ReturnHome/src/com/example/returnhome/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.returnhome;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Button button = (Button)findViewById(R.id.button1);
+		button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent();
+				intent.setAction(Intent.ACTION_MAIN);
+				intent.addCategory(Intent.CATEGORY_HOME);
+				startActivity(intent);
+			}
+		});
+	}
+}
