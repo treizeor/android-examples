cd991b15c95bac4ee073e5a45f2cdee08b8cc78e
treizeor <huabao.z@163.com>
2014-09-24 12:54:52 +0800

    记录应用的使用次数

---
+++ 疯狂Android讲义/第8章/UseCount/src/com/example/usecount/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.usecount;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	SharedPreferences preferences;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		preferences = getSharedPreferences("count", MODE_PRIVATE); // 将保存为count.xml
+		// 读取SharedPreferences里的count数据
+		int count = preferences.getInt("count", 0);
+		// 显示程序以前使用的次数
+		Toast.makeText(MainActivity.this, "程序以前被使用了"+count+"次", 0).show();
+		Editor editor = preferences.edit();
+		// 存入数据
+		editor.putInt("count", ++count);
+		editor.commit();
+	}
+}
